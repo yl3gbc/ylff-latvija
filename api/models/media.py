26d2481621a0af7de48efc114f0dfa683afeae0e
Arnis Armans <yl3gbc@yl3gbc.lv>
2026-07-11 21:12:33 +0000

    Use DuckDB sequence for media file IDs

--- api/models/media.py
+++ api/models/media.py
@@ -1,12 +1,22 @@
 from datetime import datetime
 
+from sqlalchemy import Sequence
+
 from extensions import db
+
+
+media_file_id_seq = Sequence("media_files_id_seq")
 
 
 class MediaFile(db.Model):
     __tablename__ = "media_files"
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(
+        db.Integer,
+        media_file_id_seq,
+        server_default=media_file_id_seq.next_value(),
+        primary_key=True,
+    )
 
     filename = db.Column(db.String(255), nullable=False)
 
